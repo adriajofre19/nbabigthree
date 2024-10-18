1e5edfc4d166eae4e77f83ab8f11f3278c4083b3
adriajofre19 <118483942+adriajofre19@users.noreply.github.com>
2024-10-18 17:38:16 +0200

    actions

--- resources/js/Pages/MyTeam.vue
+++ resources/js/Pages/MyTeam.vue
@@ -134,7 +134,7 @@ const titulars = ref(props.players.filter(player => player.role === 'titular'));
                     <!-- Table -->
                     <div class="w-full mx-auto bg-white shadow-lg rounded-sm border border-gray-200 mt-8">
                         <header class="px-5 py-4 border-b border-gray-100">
-                            <h2 class="font-semibold text-gray-800">Jugadores</h2>
+                            <h2 class="font-semibold text-gray-800">Jugadors</h2>
                         </header>
                         <div class="p-3">
                             <div class="overflow-x-auto">
