f4d683581b521749045b7b5d90d53dbd4e2d6989
unknown <adria.ordis@gmail.com>
2024-10-21 12:40:21 +0200

    a

--- resources/js/Pages/CrudUsers.vue
+++ resources/js/Pages/CrudUsers.vue
@@ -28,6 +28,7 @@ console.log(props.users);
       <table class="min-w-full bg-white">
         <thead>
           <tr>
+            <th class="py-2 px-4 border-b text-left">ID</th>
             <th class="py-2 px-4 border-b text-left">Nombre</th>
             <th class="py-2 px-4 border-b text-left">Equipo</th>
             <th class="py-2 px-4 border-b text-left">Posición</th>
@@ -38,6 +39,7 @@ console.log(props.users);
         </thead>
         <tbody>
           <tr v-for="user in users" :key="user.id">
+            <td class="py-2 px-4 border-b">{{ user.id }}</td>
             <td class="py-2 px-4 border-b">{{ user.name }}</td>
             <td class="py-2 px-4 border-b">{{ user.email }}</td>
             <td class="py-2 px-4 border-b">{{ user.role }}</td>
