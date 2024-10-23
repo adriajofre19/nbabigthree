ab0e59b5d6aa926a3f7af6ce125f9b3fe4c4011d
unknown <adria.ordis@gmail.com>
2024-10-23 17:43:00 +0200

    a

--- app/Http/Controllers/PlayersController.php
+++ app/Http/Controllers/PlayersController.php
@@ -29,7 +29,7 @@ class PlayersController extends Controller
         $titulars = Player::where('user_id', auth()->id())->where('role', 'titular')->get();
 
         foreach ($titulars as $titular) {
-            $titular->stats = Player::getInfoFromThisPlayerByThisWeek($titular->player_code);
+            $titular->stats = Player::getPointsFromThisPlayerOnThisWeek($titular->player_code);
         }
 
         return Inertia::render('MyTeam', [
@@ -107,7 +107,7 @@ class PlayersController extends Controller
         $stats = Player::getAllInfoFromTheApi();
 
         foreach ($players as $player) {
-            $player->stats = Player::getPointsFromThisPlayer($player->player_code);
+            $player->stats = Player::getPointsFromThisPlayerOnThisWeek($player->player_code);
        
         }
 
--- app/Models/Player.php
+++ app/Models/Player.php
@@ -70,6 +70,34 @@ class Player extends Model
     return $totalPoints;
 }
 
+public static function getPointsFromThisPlayerOnThisWeek($player_code)
+{
+    $stats = self::getAllInfoFromTheApi();
+    $totalPoints = 0;
+
+    $today = date('Y-m-d');
+    $startWeek = date('Y-m-d', strtotime('last monday', strtotime($today)));
+    $endWeek = date('Y-m-d', strtotime('next sunday', strtotime($today)));
+
+    foreach ($stats as $game) {
+        if ($game['player'] === $player_code && $game['date'] >= $startWeek && $game['date'] <= $endWeek) {
+            $totalPoints += $game['field_goals'] * 2; // Cada field_goal suma 2 puntos
+            $totalPoints -= $game['field_goals_attempted']; // Cada field_goal_attempted resta 1 punto
+            $totalPoints += $game['three_point_field_goals']; // Cada three_point_field_goal suma 1 punto
+            $totalPoints += $game['free_throws']; // Cada free_throw suma 1 punto
+            $totalPoints -= $game['free_throws_attempted']; // Cada free_throw_attempted resta 1 punto
+            $totalPoints += $game['total_rebounds']; // Cada total_rebound suma 1 punto
+            $totalPoints += $game['assists'] * 2; // Cada assist suma 2 puntos
+            $totalPoints += $game['steals'] * 4; // Cada steal suma 4 puntos
+            $totalPoints += $game['blocks'] * 4; // Cada block suma 4 puntos
+            $totalPoints -= $game['turnovers'] * 2; // Cada turnover resta 2 puntos
+            $totalPoints += $game['points']; // Cada punto suma 1 punto
+        }
+    }
+
+    return $totalPoints;
+}
+
     public static function getInfoFromThisPlayer($player_code)
     {
         $stats = self::getAllInfoFromTheApi();
@@ -89,7 +117,9 @@ class Player extends Model
         $startWeek = date('Y-m-d', strtotime('last monday', strtotime($today)));
         $endWeek = date('Y-m-d', strtotime('next sunday', strtotime($today)));
 
-        $stats = collect($stats)->where('player', $player_code)->where('date', '>=', $startWeek)->where('date', '<=', $endWeek);
+        $stats = collect($stats)->where('player', $player_code)->whereBetween('date', [$startWeek, $endWeek]);
+
+        dd($stats);
 
         $totalPoints = $stats->sum('game_score');
 
