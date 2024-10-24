05042c2a7a066c8221bb8226ebe7b3041472528c
unknown <adria.ordis@gmail.com>
2024-10-24 14:10:44 +0200

    aa

--- resources/js/Pages/TeamFromPlayer.vue
+++ resources/js/Pages/TeamFromPlayer.vue
@@ -126,7 +126,7 @@ const submit = () => {
                             <div class="flex justify-center">
                             <div v-for="player in players" :key="player.id" class="mt-4">
                                 <div v-if="player.role === 'suplente'" class="text-center w-10 bg-red-400 py-1 text-white rounded-lg">{{ player.stats }}</div>
-                                <img v-if="player.role === 'suplente'" :src="player.avatar" class="w-20 h-auto mx-auto"/>
+                                <img v-if="player.role === 'suplente'" @click="openChangePlayerModal(player.id)" :src="player.avatar" class="w-20 h-auto mx-auto"/>
                                 <div v-if="player.role === 'suplente'" class="text-center bg-gray-400 etxt-black py-1">{{ getFirstName(player.name) }}</div>
                             </div>
                         </div>
