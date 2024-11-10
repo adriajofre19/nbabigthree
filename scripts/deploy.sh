2858815b658a2d21161d8473491aa3551544624a
unknown <adria.ordis@gmail.com>
2024-11-10 12:38:42 +0100

    aaa

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 set -x
 set -e
 echo "Deploying the application"
-git restore package-lock.json
 git pull
 composer install
 npm install
 npm update
 npm run build
 php artisan migrate --force
+git restore package-lock.json
